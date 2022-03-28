417111838ed48b2aae7be3464c4653353a90e909
Camille Barbit <barbitcamille@gmail.com>
2022-03-28 12:14:21 +0200

    Added main struct

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 10:57:03 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/03/28 11:52:57 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/03/28 12:11:00 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,17 @@ LIBRARIES
 #include <unistd.h>
 #include <stdio.h>
 
+/*
+MAIN STRUCTURE
+*/
+typedef struct s_rules  t_rules;
+struct s_rules
+{
+	int	nb_philo; //also corresponds to the nb of forks
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	times_must_eat;
+};
 
 #endif
