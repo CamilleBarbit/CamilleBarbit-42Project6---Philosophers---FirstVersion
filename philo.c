d0056d52b8c4a259d90f6b7e3063b2122d2f2399
Camille Barbit <barbitcamille@gmail.com>
2022-03-28 16:44:18 +0200

    Working Makefile

--- philo.c
+++ philo.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/28 16:20:33 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/03/28 16:20:35 by camillebarb      ###   ########.fr       */
+/*   Created: 2022/03/28 16:36:26 by camillebarb       #+#    #+#             */
+/*   Updated: 2022/03/28 16:40:58 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char **argv)
 {
 	t_rules *rules;
 
+	(void)rules;
 	if (argc != 5 && argc != 6)
 		return(error("Wrong number of arguments"), 1);
 	if (parsing(argc, argv) == 1)
