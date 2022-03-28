79e52a9cc1eb71d708f942fe56cfa9eb80cef4df
Camille Barbit <barbitcamille@gmail.com>
2022-03-28 18:15:49 +0200

    Init basics info

--- init_structs.c
+++ init_structs.c
@@ -6,20 +6,24 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:36:51 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/03/28 16:36:53 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/03/28 18:15:34 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_basics(t_rules *rules, char **argv)
+int	init_basics(t_rules *rules, char **argv)
 {
 	rules->nb_philo = atoi(argv[1]);
 	rules->time_to_die = atoi(argv[2]);
 	rules->time_to_eat = atoi(argv[3]);
 	rules->time_to_sleep = atoi(argv[4]);
+	rules->times_must_eat = 0;
 	if (argv[5])
 		rules->times_must_eat = atoi(argv[5]);
-	else
-		rules->times_must_eat = 0;
+	if (rules->nb_philo < 1 || rules->nb_philo > 32768 || rules->time_to_die < 0
+		|| rules->time_to_eat < 0 || rules->time_to_sleep < 0 || rules->times_must_eat < 0)
+		return (1);
+	init_philo(rules, philo);
+	return (0);	
 }
