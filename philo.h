ef3ce0fe21af0d7bca7bc8de2ec3529d85be65bc
Camille Barbit <barbitcamille@gmail.com>
2022-03-28 18:16:17 +0200

    created structures

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 10:57:03 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/03/28 16:28:20 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/03/28 17:52:20 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,18 @@ LIBRARIES
 #include <stdlib.h>
 
 /*
-MAIN STRUCTURE
+MAIN STRUCTURES
 */
+typedef struct s_philo t_philo;
+struct s_philo
+{
+	int	philo_id;
+	int	left_fork_id;
+	int	right_fork_id;
+	int	times_eaten; //if there is a times_must_eat argument, we need to mornitor how many times each has eaten
+	int	times_last_meal;
+}
+
 typedef struct s_rules  t_rules;
 struct s_rules
 {
