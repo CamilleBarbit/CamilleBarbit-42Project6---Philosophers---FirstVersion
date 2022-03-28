516932b0164fbeee553b02a8d751bad6cb4ed8dd
CamilleBarbit <85878035+CamilleBarbit@users.noreply.github.com>
2022-03-28 11:56:01 +0200

    Add files via upload

---
+++ philo.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 11:00:03 by camillebarb       #+#    #+#             */
+/*   Updated: 2022/03/28 11:51:30 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 5 && argc != 6)
+        return(error("Wrong number of arguments"), 1);
+}
---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 10:57:03 by camillebarb       #+#    #+#             */
+/*   Updated: 2022/03/28 11:52:57 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/*
+LIBRARIES
+*/
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+#endif
