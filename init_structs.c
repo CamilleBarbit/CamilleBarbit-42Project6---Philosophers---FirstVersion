f7ff9aff8f7acc364febf431c599ac6c74c83e3b
Camille Barbit <barbitcamille@gmail.com>
2022-03-30 16:07:39 +0200

    Updated version

--- init_structs.c
+++ init_structs.c
@@ -6,24 +6,47 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:36:51 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/03/30 10:37:08 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/03/30 16:04:30 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
 
 int	init_mutexes(t_rules *rules)
 {
 	int	i;
 	
 	i = 0;
+	if (!(rules->forks = malloc(sizeof(pthread_mutex_t) * rules->nb_philos)))
+		return (error("Malloc failed"), 1);
 	while (i < rules->nb_philos)
 	{
-		if (pthread_mutext_init(&rules->forks[i], NULL) != 0)
-			return (1);
+		if (pthread_mutex_init(&rules->forks[i], NULL) != 0)
+			return (error("Failed to init mutexes"), 1);
 		i++;
-	} //So far, I have just initialized the 'forks'
+	}
+	return (0);
+}
+
+int	init_philos(t_rules *rules)
+{
+	int	i;
+
+	i = 0;
+	while (i < rules->nb_philos)
+	{
+		rules->all_philos[i].philo_id = i + 1;
+		rules->all_philos[i].left_fork_id = i;
+		if (i == 0)
+			rules->all_philos[i].right_fork_id = rules->nb_philos - 1;
+		else
+			rules->all_philos[i].right_fork_id = i - 1;
+		rules->all_philos[i].times_eaten = 0;
+		// rules->all_philos[i].time_last_meal == //???
+		// if (pthread_create(rules->all_philos[i].philo)... != 0))
+		// 	return (1);
+	}
+	return (0);
 }
 
 int	init_basics(t_rules *rules, char **argv)
@@ -34,18 +57,18 @@ int	init_basics(t_rules *rules, char **argv)
 	rules->time_to_sleep = atoi(argv[4]);
 	if (rules->nb_philos < 1 || rules->nb_philos > 32768 || rules->time_to_die <= 0
 		|| rules->time_to_eat <= 0 || rules->time_to_sleep <= 0)
-		return (1);
+		return (error("Invalid argument value"), 1);
 	if (argv[5])
 	{
 		rules->times_must_eat = atoi(argv[5]);
 		if (rules->times_must_eat <= 0)
-			return (1);	
+			return (error("Invalid argument value"), 1);	
 	}
 	else
 		rules->times_must_eat = 0;
-	rules->all_philos = malloc(sizeof(t_philo) * rules->nb_philos);
-	if (!rules->all_philos)
-		return (1);
+	if (!(rules->all_philos = malloc(sizeof(t_philo) * rules->nb_philos)))
+		return (error("Malloc failed"), 1);
+	init_mutexes(rules);
 	//I must init the mutexes before trying to init the philos
 	init_philos(rules); //In theory, then, I fill the table rules->all_philos
 	return (0);	
