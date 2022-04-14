d0212c3b25539c615536554526c17199d116f98b
Camille Barbit <barbitcamille@gmail.com>
2022-04-14 11:50:32 +0200

    Is it clearer?

--- all_actions.c
+++ all_actions.c
@@ -6,33 +6,30 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 14:02:20 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/04/14 11:20:50 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/04/14 11:48:25 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.c"
 
-void	usleep_eat_think(t_rules *rules, double time)
-{
-	double i;
-
-	i = get_time();
-	while (rules->are_dead == 0 && get_time() - i < time)
-		usleep(50);
-}
-
 int	philo_is_eating(t_philo *philo, t_rules *rules)
 {
 	if (grab_forks(philo, rules) == 1)
 		return (1);
-	action(rules, philo, "is eating");
-	philo->status = 0; //il mange //est-ce important?
-	philo->time_last_meal = get_time(); //Update heure du début de dernier repas
-	usleep_eat_think(rules, rules->time_to_eat);
-	philo->times_eaten++;
+	if (rules->time_last_meal + rules->time_to_eat < rules->time_to_die)
+	{
+		action(rules, philo, "is eating");
+		philo->status = 0; //il mange //est-ce important?
+		philo->time_last_meal = get_time(); //Update heure du début de dernier repas
+		usleep_eat_think(rules, rules->time_to_eat);
+		philo->times_eaten++;
+	}
+	else
+		usleep_eat_think(rules, rules->time_to_die - get_diff(rules->time_last_meal))
+		//écrire une fonction genre I am dead -> où il modifie la variable en question 
+	//if (philo->times_eaten == rules->times_must_eat) //we will have to do something
 	if (drop_forks(philo, rules) == 1)
 		return (1);
-	//if (philo->times_eaten == rules->times_must_eat) //we will have to do something
 	return (0);
 }
 
@@ -51,7 +48,7 @@ void	*ft_start_daily_routine(void *arg)
 	philo = (t_philo*)arg;
 	rules = philo->rules;
 
-	while(philo->is_alive == false && eaten_enough(philo, rules) != 0)
+	while(*(philo->is_dead) == false && eaten_enough(philo, rules) != 0)
 	{
 		if (philo_is_eating(philo, rules) == 1)
 			return (NULL);
@@ -61,3 +58,11 @@ void	*ft_start_daily_routine(void *arg)
 	}
 	return (NULL);
 }
+
+/*
+
+*(philo->id_dead) : philo->is_dead est une adresse, 
+donc pour avoir la valeur de la variable stockée à cette adresse, 
+je mets * devant!
+
+*/
