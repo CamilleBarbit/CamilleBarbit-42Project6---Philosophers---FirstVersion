07aa5df67fac3a54e6590eb1f84ba6424e202e81
Camille Barbit <barbitcamille@gmail.com>
2022-04-14 11:50:57 +0200

    Is it clearer?

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 14:15:36 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/04/14 11:21:12 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/04/14 11:49:22 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,30 @@ double	get_time()
 	return (time_now);
 }
 
-int eaten_enough(t_philo *philo, t_rules *rules)
+int	eaten_enough(t_philo *philo, t_rules *rules)
 {
-    if (rules->times_must_eat == -1)
-        return (1);
-    else if (philo->times_eaten < rules->times_must_eat)
-        return (1);
-    return (0);
+	if (rules->times_must_eat == -1)
+		return (1);
+	else if (philo->times_eaten < rules->times_must_eat)
+		return (1);
+	return (0);
+}
+
+double	get_diff(double time)
+{
+	double  i;
+	double  diff;
+
+	i = get_time();
+	diff = i - time;
+	return (diff);
+}
+
+void	usleep_eat_think(t_rules *rules, double time)
+{
+	double i;
+
+	i = get_time(); //l'heure à laquelle je commence mon action
+	while (get_time() - i < time)
+		usleep(50);
 }
