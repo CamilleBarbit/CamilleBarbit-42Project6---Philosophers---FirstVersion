88cbf02bbbf39cd01457bf243cfda1ba4e6a6a24
Camille BARBIT <cbarbit@e3r5p22.clusters.42paris.fr>
2022-04-18 18:32:41 +0200

    Updated

--- messages.c
+++ messages.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:32:55 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/04/18 18:02:15 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/04/18 18:31:47 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,26 @@ void	error(char *str)
 
 int	action(t_rules *rules, t_philo *philo, char *str)
 {
-	// pthread_mutex_lock(philo->dead);
-	// if (rules->are_dead == true)
-	// {
-	// 	pthread_mutex_unlock(philo->dead);
-	// 	return (1);
-	// }
-	// pthread_mutex_unlock(philo->dead);
+	if (pthread_mutex_lock(philo->state) != 0)
+		return (1);
+	if (rules->are_dead == true)
+	{
+		//printf("value is dead: %i\n", rules->are_dead);
+		//printf("over\n");
+		pthread_mutex_unlock(philo->state);
+		return (1);
+	}
+	printf("COUCOU\n");
+	printf("[%ld] ", (get_time() - rules->start_time)); //pour calculer le timestamp
+	printf("%d ", philo->philo_id);
+	printf("%s\n", str);
+	if (pthread_mutex_unlock(philo->state) != 0)
+		return (1);
+	return (0);
+}
+
+int	action_dead(t_rules *rules, t_philo *philo, char *str)
+{
 	if (pthread_mutex_lock(philo->state) != 0)
 		return (1);
 	printf("[%ld] ", (get_time() - rules->start_time)); //pour calculer le timestamp
