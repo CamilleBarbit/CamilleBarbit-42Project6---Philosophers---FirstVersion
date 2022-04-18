63424e795f1eabc3d44dcfbc0eddd5caff948bf0
Camille BARBIT <cbarbit@e3r5p22.clusters.42paris.fr>
2022-04-18 16:07:26 +0200

    Updated

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:36:26 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/04/18 11:55:06 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/04/18 16:06:12 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,19 @@ int	dispatch_philos(t_rules *rules)
 {
 	int	i;
 
-	i = 0;
 	rules->start_time = get_time();
+	i = 0;
 	while (i < rules->nb_philos)
 	{
 		rules->all_philos[i].time_last_meal = rules->start_time;
-		if (pthread_create(&rules->all_philos[i].philo, NULL, &ft_start_daily_routine, &rules->all_philos[i]) != 0)
+		if (pthread_create(&rules->all_philos[i].philo, NULL, ft_start_daily_routine, &rules->all_philos[i]) != 0)
 			return (1);
-		usleep(100); //à voir si c'est utile
+		//usleep(100); //à voir si c'est utile
 		i++;
 	}
-	if (pthread_create(&rules->checker, NULL, &ft_check_threads, &rules) != 0)
-		return (1);
 	i = 0;
 	while (i < rules->nb_philos)
 		if (pthread_join(rules->all_philos[i++].philo, NULL) != 0)
-			return (1);
-	if (pthread_join(rules->checker, NULL) != 0)
 			return (1);
 	return (0);
 }
