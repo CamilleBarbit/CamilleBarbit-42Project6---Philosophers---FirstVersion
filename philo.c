cd89e8d97f577d5a982bdd437b30c1fe5892b988
Camille BARBIT <cbarbit@e3r5p19.clusters.42paris.fr>
2022-04-20 14:44:30 +0200

    Norminette OK

--- philo.c
+++ philo.c
@@ -6,13 +6,13 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:36:26 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/04/19 11:43:07 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/04/20 14:44:03 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	dispatch_philos(t_rules *rules)
+int	odd_philos(t_rules *rules)
 {
 	int	i;
 
@@ -21,20 +21,38 @@ int	dispatch_philos(t_rules *rules)
 	while (i < rules->nb_philos)
 	{
 		rules->all_philos[i].time_last_meal = rules->start_time;
-		if (pthread_create(&rules->all_philos[i].philo, NULL, ft_start_daily_routine, &rules->all_philos[i]) != 0)
+		if (pthread_create(&rules->all_philos[i].philo, NULL,
+				ft_start_daily_routine, &rules->all_philos[i]) != 0)
 			return (1);
-		usleep(100); //à voir si c'est utile
+		usleep(100);
 		i += 2;
 	}
+	return (0);
+}
+
+int	even_philos(t_rules *rules)
+{
+	int	i;
+
 	i = 1;
 	while (i < rules->nb_philos)
 	{
 		rules->all_philos[i].time_last_meal = rules->start_time;
-		if (pthread_create(&rules->all_philos[i].philo, NULL, ft_start_daily_routine, &rules->all_philos[i]) != 0)
+		if (pthread_create(&rules->all_philos[i].philo, NULL,
+				ft_start_daily_routine, &rules->all_philos[i]) != 0)
 			return (1);
-		usleep(100); //à voir si c'est utile
+		usleep(100);
 		i += 2;
 	}
+	return (0);
+}
+
+int	dispatch_philos(t_rules *rules)
+{
+	int	i;
+
+	if ((odd_philos(rules) == 1) || (even_philos(rules) == 1))
+		return (1);
 	if (pthread_create(&rules->checker, NULL, ft_check_threads, rules) != 0)
 		return (1);
 	i = 0;
@@ -46,17 +64,16 @@ int	dispatch_philos(t_rules *rules)
 	return (0);
 }
 
-	
 int	main(int argc, char **argv)
 {
-	t_rules *rules;
-	//int		i;
-	
+	t_rules	*rules;
+
 	if (argc != 5 && argc != 6)
-		return(error("Wrong number of arguments\n"), 1);	
+		return (error("Wrong number of arguments\n"), 1);
 	if (parsing(argc, argv) == 1)
 		return (error("Invalid argument(s)\n"), 1);
-	if (!(rules = malloc(sizeof(t_rules))))
+	rules = malloc(sizeof(t_rules));
+	if (!rules)
 		return (error("Malloc failed\n"), 1);
 	if (init_basics(rules, argv) == 1)
 		return (error("Failed initialising variables\n"), 1);
