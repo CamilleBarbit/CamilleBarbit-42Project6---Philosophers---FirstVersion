091e3892cf0463344e1b91b1b0af6cdf315133df
Camille BARBIT <cbarbit@e2r1p8.clusters.42paris.fr>
2022-04-22 16:38:13 +0200

    working

--- check_philo_state.c
+++ check_philo_state.c
@@ -6,13 +6,13 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:00:06 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/04/22 14:37:48 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/04/22 16:37:50 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	check_meals(t_philo *philo, t_rules *rules)
+static int	check_meals(t_philo *philo, t_rules *rules)
 {
 	pthread_mutex_lock(&philo->nb_meals);
 	if (philo->times_eaten == rules->times_must_eat)
@@ -21,14 +21,29 @@ int	check_meals(t_philo *philo, t_rules *rules)
 	return (0);
 }
 
-void	modify_death_status(t_rules *rules)
+static void	modify_death_status(t_rules *rules)
 {
 	pthread_mutex_lock(&rules->have_died);
 	if (rules->are_dead == false)
 		rules->are_dead = true;
 	pthread_mutex_unlock(&rules->have_died);
 }
-			
+
+static	int	routine_thread(t_philo *philo, t_rules *rules)
+{
+	if ((get_diff(philo->time_last_meal)
+			>= rules->time_to_die))
+	{
+		pthread_mutex_unlock(&philo->state);
+		pthread_mutex_unlock(&philo->time_eat);
+		modify_death_status(rules);
+		if (action_dead(rules, philo, "died") == 1)
+			return (1);
+		return (1);
+	}
+	return (0);
+}
+
 void	*ft_check_threads(void *arg)
 {
 	t_rules	*rules;
@@ -44,15 +59,8 @@ void	*ft_check_threads(void *arg)
 				return (NULL);
 			pthread_mutex_lock(&rules->all_philos[i].time_eat);
 			pthread_mutex_lock(&rules->all_philos[i].state);
-			if ((get_diff(rules->all_philos[i].time_last_meal) >= rules->time_to_die))
-			{
-				pthread_mutex_unlock(&rules->all_philos[i].state);
-				pthread_mutex_unlock(&rules->all_philos[i].time_eat);
-				modify_death_status(rules);
-				if (action_dead(rules, &rules->all_philos[i], "died") == 1)
-					return (NULL);
+			if (routine_thread(&rules->all_philos[i], rules) == 1)
 				return (NULL);
-			}
 			pthread_mutex_unlock(&rules->all_philos[i].state);
 			pthread_mutex_unlock(&rules->all_philos[i].time_eat);
 			i++;
